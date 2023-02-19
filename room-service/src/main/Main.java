9beeac91520857c138b3ebb9b9c9a1b7bc2951b7
FilippoGurioli <93600198+FilippoGurioli@users.noreply.github.com>
2023-02-19 15:15:39 +0100

    Create java project for room service

---
+++ room-service/src/main/Main.java
@@ -0,0 +1,14 @@
+package main;
+
+import roomService.RoomService;
+
+/**
+ * Application starting point.
+ */
+public class Main {
+
+	public static void main(String[] args) {
+		new RoomService();
+	}
+
+}
---
+++ room-service/src/roomService/RoomService.java
@@ -0,0 +1,8 @@
+package roomService;
+
+/**
+ * Application main class, it contains the main logic of information exchanging between other subsystems.
+ */
+public class RoomService {
+
+}
