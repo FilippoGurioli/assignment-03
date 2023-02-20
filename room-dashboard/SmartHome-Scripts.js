24f8ff10853a6e41e758ea07a5b7b4f1a7024edb
Silvia Furegato - silvia.furegato@studio.unibo.it <silvia.furegato@studio.unibo.it>
2023-02-20 16:32:27 +0100

    Added states history section in dashboard

--- room-dashboard/SmartHome-Scripts.js
+++ room-dashboard/SmartHome-Scripts.js
@@ -1,5 +1,7 @@
 let btnLights = document.getElementById("lights");
 let rngBlinds = document.getElementById("blinds");
+let btnControls = document.getElementById("btnControls");
+let btnHistory = document.getElementById("btnHistory");
 
 setup("ON", 90);
 
@@ -13,6 +15,16 @@ btnLights.addEventListener("click", function(event){
 
 rngBlinds.addEventListener("change", function(event){
     document.querySelector("div p").innerHTML = rngBlinds.value;
+});
+
+btnControls.addEventListener("click", function(event){
+    document.getElementById("controls").style.display = "flex";
+    document.getElementById("state").style.display = "none";
+});
+
+btnHistory.addEventListener("click", function(event){
+    document.getElementById("controls").style.display = "none";
+    document.getElementById("state").style.display = "flex";
 });
 
 function setup(light, degrees) {
