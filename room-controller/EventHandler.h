840e1d6eee1093f06cb8b66a1367f7aa6151b96e
FilippoGurioli <93600198+FilippoGurioli@users.noreply.github.com>
2023-02-22 16:06:10 +0100

    Fully functioning async FSM with every component

---
+++ room-controller/EventHandler.h
@@ -0,0 +1,74 @@
+#ifndef __EVENTHANDLER__
+#define __EVENTHANDLER__
+
+#include "async_fsm.h"
+#include "Led.h"
+#include "SerialPortImpl.h"
+
+#include <Servo.h>
+#include <ctype.h>
+
+#define BUFFER_SIZE 40
+#define MAX_COMMANDS 5
+
+class EventHandler : public AsyncFSM {
+
+  private:
+    Led* led;
+    SerialPort* comPort;
+    char buffer[BUFFER_SIZE];
+    Servo servo;
+
+  public:
+    EventHandler(SerialPort* comPort, Led* led, Servo servo){
+      this->comPort = comPort;
+      this->comPort->registerObserver(this);
+      this->led = led;
+      led->switchOff();
+      this->servo = servo;
+    }
+  
+    void handleEvent(Event* ev) {
+      String commands[MAX_COMMANDS];
+      bool flag = true;
+      int j = 0;
+      int rlen = Serial.readBytes(buffer, BUFFER_SIZE);
+      String info = String(buffer);
+      //Communication Protocol: remove white spaces and separate different instructions
+      String formatted;
+      for (int i = 0; i < info.length(); i++) {
+        if (info.charAt(i) != ' ' && info.charAt(i) != '\n') {
+          formatted += info.charAt(i);
+        }
+        if (info.charAt(i) == '\n') {
+          commands[j++] = formatted;
+          formatted = "";
+        }
+      }
+      //--------------------end of CommP----------------------------------------------
+
+      for (int i = 0; i < MAX_COMMANDS && commands[i] != NULL; i++) {
+        for (j = 1; j < commands[i].length(); j++) {
+          if (!isDigit(commands[i].charAt(j))) {
+            flag = false;
+          }
+        }
+        if (flag && (isDigit(commands[i].charAt(0)) || commands[i].charAt(0) == '-')) {
+          int val = commands[i].toInt();
+          val = (val >= 0 ? (val <= 180 ? val : 180) : 0);
+          Serial.println("Servo rotation");
+          servo.write(val);
+        } else if (commands[i] == "ON") {
+          Serial.println("Turning on led");
+          led->switchOn();
+        } else if (commands[i] == "OFF") {
+          Serial.println("Turning off led");
+          led->switchOff();
+        } else {
+          Serial.println("This command does not exist, try again...");
+        }
+      }
+    }
+};
+
+#endif
--- room-controller/room-controller.ino
+++ room-controller/room-controller.ino
@@ -1,82 +1,22 @@
 #include "async_fsm.h"
 #include "Led.h"
 #include "SerialPortImpl.h"
+#include "EventHandler.h"
 
 #include <Servo.h>
-#include <ctype.h>
 
 #define SERIAL_IN_PIN 0
 #define SERVOPIN 3
 #define LEDPIN 13
-
-#define BUFFER_SIZE 40
-#define MAX_COMMANDS 5
-
-class EventHandler : public AsyncFSM {
-  public:
-    EventHandler(SerialPort* comPort, Led* led){
-      this->comPort = comPort;
-      this->comPort->registerObserver(this);
-      this->led = led;
-      led->switchOff();
-      myservo.attach(SERVOPIN);
-    }
-  
-    void handleEvent(Event* ev) {
-      String commands[MAX_COMMANDS];
-      bool flag = true;
-      int j = 0;
-      int rlen = Serial.readBytes(buffer, BUFFER_SIZE);
-      String info = String(buffer);
-      //Communication Protocol: remove white spaces and separate different instructions
-      String formatted;
-      for (int i = 0; i < info.length(); i++) {
-        if (info.charAt(i) != ' ' && info.charAt(i) != '\n') {
-          formatted += info.charAt(i);
-        }
-        if (info.charAt(i) == '\n') {
-          commands[j++] = formatted;
-          formatted = "";
-        }
-      }
-      //--------------------end of CommP----------------------------------------------
-
-      for (int i = 0; i < MAX_COMMANDS && commands[i] != NULL; i++) {
-        for (j = 1; j < commands[i].length(); j++) {
-          if (!isDigit(commands[i].charAt(j))) {
-            flag = false;
-          }
-        }
-        if (flag && (isDigit(commands[i].charAt(0)) || commands[i].charAt(0) == '-')) {
-          int val = commands[i].toInt();
-          val = (val >= 0 ? (val <= 180 ? val : 180) : 0);
-          Serial.println("Servo rotation");
-          myservo.write(val);
-        } else if (commands[i] == "ON") {
-          Serial.println("Turning on led");
-          led->switchOn();
-        } else if (commands[i] == "OFF") {
-          Serial.println("Turning off led");
-          led->switchOff();
-        } else {
-          Serial.println("This command does not exist, try again...");
-        }
-      }
-    }
-
-  private:
-    Led* led;
-    SerialPort* comPort;
-    char buffer[BUFFER_SIZE];
-    Servo myservo;
-};
 
 EventHandler* eventHandler;
 
 void setup() {
   Led* led = new Led(LEDPIN);
   SerialPort* comPort = new SerialPortImpl(SERIAL_IN_PIN);
-  eventHandler = new EventHandler(comPort, led);
+  Servo servo;
+  servo.attach(SERVOPIN);
+  eventHandler = new EventHandler(comPort, led, servo);
 }
 
 void loop() {
