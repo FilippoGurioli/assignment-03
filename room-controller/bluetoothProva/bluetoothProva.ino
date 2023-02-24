26411a1abc0f542956bc780da1f798e1ca85c712
FilippoGurioli <93600198+FilippoGurioli@users.noreply.github.com>
2023-02-24 19:50:26 +0100

    Self sharing a test file, in the next commits it will be deleted

---
+++ room-controller/bluetoothProva/bluetoothProva.ino
@@ -0,0 +1,48 @@
+const int ledPin = 7; // Built in LED in Arduino board
+String msg,cmd;
+
+
+#include "AltSoftSerial.h"
+#include <ServoTimer2.h>
+
+AltSoftSerial channel;
+ServoTimer2 servo;
+
+void setup() {
+  // Initialization
+  pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, LOW);
+  Serial.begin(9600); // Communication rate of the Bluetooth Module
+  channel.begin(9600);
+  servo.attach(5);
+  msg = "";
+}
+
+void loop() {
+  int ch;
+  while(!channel.available()) {}
+  do {
+    ch = channel.read();
+    if (ch != -1) {
+      msg += (char) ch;
+    }
+  } while(ch != '>');
+  Serial.println("Ricevuto: " + msg);
+  
+  // Control LED in Arduino board
+  if (msg == "<turn on>"){
+    digitalWrite(ledPin, HIGH); // Turn on LED
+    Serial.println("LED is turned on\n"); // Then send status message to Android
+    channel.println("LED is turned on\n");
+    msg = ""; // reset command
+    servo.write(750);
+  } else {
+    if (msg == "<turn off>"){
+      digitalWrite(ledPin, LOW); // Turn off LED
+      Serial.println("LED is turned off\n"); // Then send status message to Android
+      channel.println("LED is turned off\n");
+      msg = ""; // reset command
+      servo.write(2250);
+    }
+  }
+}
